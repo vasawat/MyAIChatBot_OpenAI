bf956c38b7da5e40f0bf09d6302044d097d2ce66
vasawat prapachaimongkol <vasawat.ppcmk@gmail.com>
2025-01-17 14:24:31 +0700

    initial F

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ function App() {
   return (
     <div className="w-full h-screen grid place-items-center">
       <div className="w-2/3 my-6 flex flex-col gap-6">
-        <div className="w-full h-96 overflow-y-scroll bg-gray-200 p-6 rounded shadow-xl">
+        <div className="w-full h-96 overflow-y-scroll bg-gray-100 p-6 rounded shadow-xl border border-gray-300 ">
           <p className="mb-2">textBox : </p>
           <div className="flex flex-col gap-4">
             {dataTest.length > 0 &&
               dataTest.map((data, index) =>
                 index % 2 === 0 ? (
                   <div className="w-full flex justify-end items-center gap-2">
-                    <div className="text-sm bg-gray-100 p-3 max-w-[70%] rounded">
+                    <div className="text-sm bg-gray-200 p-3 max-w-[70%] rounded">
                       {data}
                     </div>
                     <p>: You</p>
@@ -31,7 +31,7 @@ function App() {
                 ) : (
                   <div className="w-full flex justify-start items-center gap-2">
                     <p>AI :</p>
-                    <div className="text-sm bg-gray-100 p-3 max-w-[70%] rounded">
+                    <div className="text-sm bg-gray-300 p-3 max-w-[70%] rounded">
                       {data}
                     </div>
                   </div>
@@ -39,12 +39,17 @@ function App() {
               )}
           </div>
         </div>
+
         <div className="w-full flex flex-col gap-6 bg-gray-200 p-6 rounded shadow-xl transition duration-300 ease-in-out">
           <Button
             onClick={() =>
               axios.get(environment.apiUrl + "/api").then((res) => {
                 console.log(res.data);
-                setDataTest([...dataTest, res.data.choices[0].message.content]);
+                setDataTest([
+                  ...dataTest,
+                  "Hi Who are you?",
+                  res.data.choices[0].message.content,
+                ]);
               })
             }
           >
